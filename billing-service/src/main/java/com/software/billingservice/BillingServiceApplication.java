aa46377ef4e2791c10ce225718b9f5d8c27074cf
rajnish kumar <kumarrajneesh290@gmail.com>
2026-01-05 20:15:44 +0530

    initialize billing service with basic configuration and structure

---
+++ billing-service/src/main/java/com/software/billingservice/BillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.software.billingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BillingServiceApplication.class, args);
+    }
+
+}
